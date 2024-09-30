12e5c279bbcfc8768512664bff4554f6b243d6bf
Agyat2003 <0126cd211019@oriental.ac.in>
2024-09-30 15:56:42 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,25 +1,43 @@
 class Solution(object):
-   def isValid(self, s):
+ def isValid(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        stack = []
-        mapping = {')': '(', ']': '[', '}': '{'}
-
-        for char in s:
-
-            if char in mapping:
-                top_element = stack.pop() if stack else '#'
-                
-          
-                if mapping[char] != top_element:
+        # This hashmap maps a closing bracket to an opening one
+        bracketsMap = {
+            ')':'(',
+            ']':'[',
+            '}':'{'
+        }
+        # Edge case, we cannot start a valid string with an closing
+        # bracket or have it with length 1
+        if len(s) == 1 or s[0] in bracketsMap:
+            return False
+        # Have astack to hold the opening brackets
+        openingBrackets = []
+        # Iterating the string
+        for index in range(len(s)):
+            # Current character
+            currentBracket = s[index]
+            if currentBracket not in bracketsMap: # It's an opening bracket
+                openingBrackets.append(currentBracket)
+                # Moving on to the next character
+                continue
+            else: # We have a closing bracket
+                # Matching bracket
+                matchingBracket = bracketsMap[currentBracket]
+                # Grabbing top bracket from stack
+                if not len(openingBrackets):
                     return False
-            else:
-                stack.append(char)
-
-        return not stack
-
+                candidateBracket = openingBrackets.pop()
+                # Comparing the two
+                if matchingBracket != candidateBracket:
+                    return False
+                else: # We have a valid string so far
+                    continue
+        # Takes care of 2 things at once. If we've iterated the entire string and have a valid string we should return True. But we also make sure that the stack is empty in the case of all '((((('
+        return not len(openingBrackets)
 
 
 
