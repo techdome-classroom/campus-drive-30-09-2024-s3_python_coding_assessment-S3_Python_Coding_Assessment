1dec2e0f7313c2badd7d316c5a618bc9424718f7
Agyat2003 <0126cd211019@oriental.ac.in>
2024-09-30 15:41:13 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,10 +1,23 @@
 class Solution(object):
-    def isValid(self, s):
+   def isValid(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        pass
+        # Use two pointers to traverse the string from left to right
+        left = 0
+        right = len(s) - 1
+        
+        # While the two pointers have not crossed each other
+        while left < right:
+            # Check for valid pairs at both ends
+            if (s[left] == '(' and s[right] == ')') or (s[left] == '{' and s[right] == '}') or (s[left] == '[' and s[right] == ']'):
+                left += 1
+                right -= 1
+            else:
+                return False
+
+        return left >= right
 
 
 
